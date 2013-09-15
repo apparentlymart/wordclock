189b712ec30dfe76e1e4b3d8cea8d6857a4f05c5
Martin Atkins <mart@degeneration.co.uk>
2013-09-15 13:17:37 -0700

    Better prototype which can actually say TWELVE.
    Got rid of the "IT IS" words; they're not very interesting anyway since
    they are just always illuminated. We can still say
    TIME TO EAT / SLEEP / GO / WAKE UP as well as actual time readouts.

--- prototype.c
+++ prototype.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <string.h>
 
-char *display = "ITHISDTIMEJAKQUARTERTOTWENTYDFIVETENHALFWAKEPASTOUPFOUREIGHTWONEGOTENINEATSIXMTHREELEVENSEVENFIVEKPSLEEPOCLOCK";
+char *display = "HALFTENTIMEACQUARTERTOTWENTYLFIVEPASTOMAFOUREIGHTWONEGOTENINEATSIXATHREELEVENTWELVENWAKESEVENFIVEUPSLEEPOCLOCK";
 char light[10][11];
 int force_hour = 0;
 int force_minute = 0;
@@ -18,6 +18,15 @@ void illuminate(int x, int y, int length) {
 
 void update(void) {
     memset(light, 0, sizeof(light));
+
+    //illuminate(7, 0, 4); // time
+    //illuminate(9, 1, 2); // to
+    //illuminate(7, 7, 4); // wake
+    //illuminate(9, 8, 2); // up
+    //illuminate(0, 9, 5); // sleep
+    //illuminate(9, 4, 2); // go
+    //illuminate(5, 5, 3); // eat
+    //return;
 
     time_t ts;
     time(&ts);
@@ -39,22 +48,22 @@ void update(void) {
 
         case 1:
         case 13:
-            illuminate(6, 5, 3);
+            illuminate(6, 4, 3);
             break;
 
         case 2:
         case 14:
-            illuminate(4, 5, 3);
+            illuminate(4, 4, 3);
             break;
 
         case 3:
         case 15:
-            illuminate(1, 7, 5);
+            illuminate(1, 6, 5);
             break;
 
         case 4:
         case 16:
-            illuminate(7, 4, 4);
+            illuminate(7, 3, 4);
             break;
 
         case 5:
@@ -64,7 +73,7 @@ void update(void) {
 
         case 6:
         case 18:
-            illuminate(8, 6, 3);
+            illuminate(8, 5, 3);
             break;
 
         case 7:
@@ -74,37 +83,37 @@ void update(void) {
 
         case 8:
         case 20:
-            illuminate(0, 5, 5);
+            illuminate(0, 4, 5);
             break;
 
         case 9:
         case 21:
-            illuminate(2, 6, 4);
+            illuminate(2, 5, 4);
             break;
 
         case 10:
         case 22:
-            illuminate(0, 6, 3);
+            illuminate(0, 5, 3);
             break;
 
         case 11:
         case 23:
-            illuminate(5, 7, 6);
+            illuminate(5, 6, 6);
             break;
 
-            /*case 12:
+        case 12:
         case 0:
-            illuminate(, , );
-            break;*/
+            illuminate(0, 7, 6);
+            break;
 
     }
 
     if (now->tm_min > 4){
         if (now->tm_min < 35) {
-            illuminate(0, 4, 4);
+            illuminate(0, 3, 4);
         }
         else {
-            illuminate(3, 4, 2);
+            illuminate(3, 3, 2);
         }
     }
 
@@ -120,7 +129,7 @@ void update(void) {
 
         case 2: // ten
         case 10:
-            illuminate(0, 3, 3);
+            illuminate(4, 0, 3);
             break;
 
         case 3: // quarter
@@ -141,7 +150,7 @@ void update(void) {
             break;
 
         case 6: // half
-            illuminate(3, 3, 4);
+            illuminate(0, 0, 4);
             break;
 
     }
@@ -175,7 +184,7 @@ int main(int argc, char **argv) {
     while (1) {
         update();
         draw();
-        sleep(30);
+        sleep(1);
     }
 
     return 0;
